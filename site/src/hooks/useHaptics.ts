e7b009d3b2e273f988b443a2316e1b1781548f9f
Lochie Axon <lochieaxon@gmail.com>
2026-03-01 01:13:31 +1100

    remove debug

--- site/src/hooks/useHaptics.ts
+++ site/src/hooks/useHaptics.ts
@@ -1,11 +1,13 @@
 import { useCallback } from "react";
 import { useWebHaptics } from "web-haptics/react";
 import type { HapticInput, TriggerOptions } from "web-haptics";
+import { useApp } from "../context/app";
 import { shakeFavicon } from "../utils/faviconShake";
 
 // abstraction so we can do some extra stuff
 export const useHaptics = () => {
-  const { trigger } = useWebHaptics({ debug: true, showSwitch: true });
+  const { debug } = useApp();
+  const { trigger } = useWebHaptics({ debug });
 
   const triggerWithShake = useCallback(
     (input?: HapticInput, options?: TriggerOptions) => {
